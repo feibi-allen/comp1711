4146dfceb2d5d5b3631e85e7c3dec084258f7158
feibi-allen <146375798+feibi-allen@users.noreply.github.com>
2023-10-23 09:41:18 +0000

    prime checker function added

--- Week3/Session2/Worksheets/Prime_checker_function.c
+++ Week3/Session2/Worksheets/Prime_checker_function.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+int is_prime(int my_number){
+    int prime = 1;
+    for (int i = 2; i < my_number; i++ ){
+        if (my_number%i == 0){
+            prime = 0;
+        }
+    }
+    return prime;
+}
+
+int main(){
+    int my_number = 7;
+    int is_it_prime = is_prime(my_number);
+    printf("%d\n", is_it_prime);
+    return 0;
+}
